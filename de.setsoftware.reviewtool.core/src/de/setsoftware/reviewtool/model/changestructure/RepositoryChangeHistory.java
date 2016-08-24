f1d780d86dc93a82facc3e91a3531f9276b9d9b1
Christoph Schulz <develop@kristov.de>
2016-08-24 10:56:13 +0200

    changestructure: added change history types
    The new types RepositoryChangeHistory and FileChangeHistory make it easy
    to manage all changes in a file or repository, respectively, with regard
    to a given revision range. Contrary to the FileDiff type, a
    FileChangeHistory does not merge the Hunks. Consequently, it is possible
    to generate different FileDiffs on-the-fly, providing different revision
    ranges. The RepositoryChangeHistory type manages FileChangeHistories for
    a given file, abstracting from path changes (renames).

---
+++ de.setsoftware.reviewtool.core/src/de/setsoftware/reviewtool/model/changestructure/RepositoryChangeHistory.java
@@ -0,0 +1,103 @@
+package de.setsoftware.reviewtool.model.changestructure;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Represents a container for {@link FileChangeHistory} objects.
+ */
+public class RepositoryChangeHistory {
+
+    /**
+     * Maps a path to a set of {@link FileInRevision}s.
+     */
+    private final Map<String, List<FileInRevision>> fileRevisions;
+    /**
+     * The change histories per file.
+     */
+    final Map<FileInRevision, FileChangeHistory> changeHistories;
+
+    /**
+     * Creates an empty change history.
+     */
+    public RepositoryChangeHistory() {
+        this.fileRevisions = new LinkedHashMap<>();
+        this.changeHistories = new LinkedHashMap<>();
+    }
+
+    /**
+     * Creates a change history from a collection of {@link Commit}s.
+     *
+     * @param commits The commits to import.
+     */
+    public RepositoryChangeHistory(final Collection<? extends Commit> commits) {
+        this();
+        for (final Commit commit : commits) {
+            for (final Change change : commit.getChanges()) {
+                change.accept(new ChangeVisitor() {
+
+                    @Override
+                    public void handle(final TextualChangeHunk visitee) {
+                        RepositoryChangeHistory.this.addChange(visitee);
+                    }
+
+                    @Override
+                    public void handle(final BinaryChange visitee) {
+                    }
+
+                });
+            }
+        }
+    }
+
+    /**
+     * Maps a {@link FileInRevision} to a {@link FileChangeHistory}.
+     * @param file The {@link FileInRevision} object.
+     * @return The {@link FileChangeHistory} describing changes for passed {@link FileInRevision} or null if not found.
+     */
+    public FileChangeHistory getHistory(final FileInRevision file) {
+        return this.changeHistories.get(file);
+    }
+
+    /**
+     * Adds a {@link TextualChangeHunk} to this change history.
+     * @param change The {@link Change} to add.
+     */
+    public void addChange(final TextualChangeHunk change) {
+        this.getChangeHistory(change.getFromFragment().getFile(),
+                change.getToFragment().getFile()).add(new Hunk(change));
+    }
+
+    /**
+     * Returns the {@link FileChangeHistory} given two {@link FileInRevision}s. The internal data structures are
+     * adjusted such that the change history is linked to the old and the new revision.
+     * @param fromFile The source file.
+     * @param toFile The target file.
+     * @return The corresponding {@link FileHistoryChange} object. If no such object exists yet, it is created and
+     * added.
+     */
+    private FileChangeHistory getChangeHistory(final FileInRevision fromFile, final FileInRevision toFile) {
+        List<FileInRevision> files = this.fileRevisions.get(fromFile.getPath());
+        if (files == null) {
+            files = new ArrayList<>();
+            files.add(fromFile);
+            this.fileRevisions.put(fromFile.getPath(), files);
+            this.changeHistories.put(fromFile, new FileChangeHistory());
+        } else if (!this.changeHistories.containsKey(fromFile)) {
+            files.add(fromFile);
+            this.changeHistories.put(fromFile, this.changeHistories.get(files.get(0)));
+        }
+        if (!this.fileRevisions.containsKey(toFile.getPath())) {
+            this.fileRevisions.put(toFile.getPath(), files);
+        }
+        if (!this.changeHistories.containsKey(toFile)) {
+            files.add(toFile);
+            this.changeHistories.put(toFile, this.changeHistories.get(fromFile));
+        }
+        return this.changeHistories.get(toFile);
+    }
+
+}
