2c46dad743aacffa3ec43e7778fcea6a161d5d08
Tobias Baum <tbaum@tntinteractive.de>
2016-10-10 22:25:19 +0200

    checkstyles

--- de.setsoftware.reviewtool.core/src/de/setsoftware/reviewtool/base/Multiset.java
+++ de.setsoftware.reviewtool.core/src/de/setsoftware/reviewtool/base/Multiset.java
@@ -9,12 +9,18 @@ import java.util.Map;
  */
 public class Multiset<T> {
 
+    /**
+     * Mutable counter value.
+     */
     private static final class Counter {
         private int cnt;
     }
 
     private final Map<T, Counter> items = new HashMap<>();
 
+    /**
+     * Add an item to the multiset.
+     */
     public void add(T item) {
         Counter c = this.items.get(item);
         if (c == null) {
@@ -24,11 +30,17 @@ public class Multiset<T> {
         c.cnt++;
     }
 
+    /**
+     * Returns true iff the given is contained at least once in the map.
+     */
     public boolean contains(T item) {
         final Counter c = this.items.get(item);
         return c != null;
     }
 
+    /**
+     * Removes one occurrence of the given item from this multiset.
+     */
     public void remove(T item) {
         final Counter c = this.items.get(item);
         if (c == null) {
