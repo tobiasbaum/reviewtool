af2992c7db1f77e0f9726fc23cb8acf8b13ca6d2
kristov <develop@kristov.de>
2018-06-26 13:54:47 +0200

    base: type deduction of partial order algorithms improved

--- de.setsoftware.reviewtool.core/src/de/setsoftware/reviewtool/base/PartialOrderAlgorithms.java
+++ de.setsoftware.reviewtool.core/src/de/setsoftware/reviewtool/base/PartialOrderAlgorithms.java
@@ -17,12 +17,12 @@ public final class PartialOrderAlgorithms {
      * If the collection of revisions passed is empty, {@code null} is returned.
      * @param elements The collection of elements where to find some minimal element.
      */
-    public static <T extends IPartiallyComparable<T>> T getSomeMinimum(final Collection<? extends T> elements) {
-        T smallestSoFar = null;
-        for (final T e : elements) {
+    public static <T extends IPartiallyComparable<T>, U extends T> U getSomeMinimum(final Collection<U> elements) {
+        U smallestSoFar = null;
+        for (final U e : elements) {
             if (smallestSoFar == null) {
                 smallestSoFar = e;
-            } else if (e.le(smallestSoFar) && !smallestSoFar.le(e)) {
+            } else if (e.le(smallestSoFar)) {
                 smallestSoFar = e;
             }
         }
@@ -34,15 +34,15 @@ public final class PartialOrderAlgorithms {
      * @param toSort The collection to be sorted topologically.
      * @return The sorted set, represented as a list.
      */
-    public static <T extends IPartiallyComparable<T>> List<T> topoSort(final Collection<? extends T> toSort) {
+    public static <T extends IPartiallyComparable<T>, U extends T> List<U> topoSort(final Collection<U> toSort) {
         if (toSort.isEmpty()) {
             return Collections.emptyList();
         }
 
-        final LinkedHashSet<T> remainingRevisions = new LinkedHashSet<>(toSort);
-        final List<T> ret = new ArrayList<>();
+        final LinkedHashSet<U> remainingRevisions = new LinkedHashSet<>(toSort);
+        final List<U> ret = new ArrayList<>();
         while (!remainingRevisions.isEmpty()) {
-            final T minimum = getSomeMinimum(remainingRevisions);
+            final U minimum = getSomeMinimum(remainingRevisions);
             ret.add(minimum);
             remainingRevisions.remove(minimum);
         }
